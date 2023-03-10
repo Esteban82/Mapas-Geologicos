be986627753edf11a0945be095287d57c4824da1
Esteban82 <federico.esteban@gmail.com>
2023-03-10 15:03:29 -0300

    Actualizar Moon

--- __Pruebas/moon_relief/moon.sh
+++ __Pruebas/moon_relief/moon.sh
@@ -2,9 +2,9 @@
 
 gmt set GMT_DATA_SERVER=test
 gmt begin moon_relief png
-	gmt grdimage @moon_relief_06m -I -Bf -Cmoon_relief2.cpt
+	gmt grdimage @moon_relief_04m -I -Bf -Cmoon_relief2.cpt
 	gmt colorbar -Ba2000
 	
-	gmt grdimage @moon_relief_06m -I -Bf -Cmoon_relief3.cpt -Y10c
+	gmt grdimage @moon_relief_04m -I -Bf -Cmoon_relief3.cpt -Y10c
 	gmt colorbar -Ba2000
 gmt end
